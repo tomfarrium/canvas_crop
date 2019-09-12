ed75fc5440723b24898cb1935dc6e03998ebed4c
Михаил Меренков <mikhail.merenckov@yandex.ru>
2019-09-12 13:17:43 +0300

    aa3

--- js/jcrop.js
+++ js/jcrop.js
@@ -1,16 +1,17 @@
-var imgPreview = document.querySelector('.img-preview img');
-
-var loadImg = function(input) {
-  if (input.files && input.files[0]) {
-    var reader = new FileReader();
-
-    reader.onload = function(e) {
-      imgPreview.src = e.target.result;
-    }
-    reader.readAsDataURL(input.files[0]);
-  }
+newFunction();
+function newFunction() {
+	var imgPreview = document.querySelector('.img-preview img');
+	var loadImg = function (input) {
+		if (input.files && input.files[0]) {
+			var reader = new FileReader();
+			reader.onload = function (e) {
+				imgPreview.src = e.target.result;
+			};
+			reader.readAsDataURL(input.files[0]);
+		}
+	};
+	document.querySelector('#img').onchange = function () {
+		loadImg(this);
+	};
 }
 
-document.querySelector('#img').onchange = function() {
-  loadImg(this);
-}
